c105166e4b8f548589409a1b37085a2c742a6045
Matteo Delucchi <mail@matteodelucchi.com>
2022-11-03 15:17:04 +0100

    provide path to save

--- Titanic/src/kaggle.jl
+++ Titanic/src/kaggle.jl
@@ -1,8 +1,8 @@
-function predict_submission(model, X_test)
+function predict_submission(model, X_test, filepath)
     passengerid = DataFrame(CSV.File("./data/test.csv"))[:,1]
     y_pred = model.predict(X_test)
 
     df_pred = DataFrame([passengerid, y_pred], ["PassengerId", "Survived"])
 
-    CSV.write("./data/submission.csv", df_pred)
+    CSV.write(filepath, df_pred)
 end
